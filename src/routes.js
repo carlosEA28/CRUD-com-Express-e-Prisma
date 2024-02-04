eeb20f1eb925e8e3244090531b0ae1a5e56d7fca
carlosEA28 <carloseaduarte28@gmail.com>
2024-02-04 13:53:48 -0300

    API CRUD COM EXPRESS, PRISMA

---
+++ src/routes.js
@@ -0,0 +1,18 @@
+import { Router } from "express";
+
+import UserController from "./controllers/UserController";
+import PostController from "./controllers/postController";
+
+const router = Router();
+
+router.post("/user", UserController.createUser);
+router.get("/users", UserController.findAllUsers);
+router.get("/user/:id", UserController.findUser);
+router.put("/user/:id", UserController.updateUser);
+router.delete("/user/:id", UserController.deleteUser);
+
+router.post("/post/user/:id", PostController.craetePost);
+router.get("/posts", PostController.FindAllPosts);
+router.put("/post/:id", PostController.UpdatePost);
+
+export { router };
